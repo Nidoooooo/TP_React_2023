dc8b34489b2f8ade20cd3f3ae2dc42461788f03d
Nicolas Quenon <134261089+Nidoooooo@users.noreply.github.com>
2024-02-29 15:15:54 +0100

    Exercice 1

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,30 @@
-
 import './App.css'
-
 function App() {
-  
-
+  const title = {
+    text1:"Hello",
+    text2:"React"
+  };
   return (
     <div>
-      <h1>Hello world</h1>
+      <Gallery />
     </div>
-  )
+  );
 }
 
 export default App
+
+const Photo = () => {
+  return(
+    <img src="https://picsum.photos/id/16/200"></img> 
+  );
+}
+
+const Gallery = () => {
+  return(
+      <section>
+        <h1>Galerie de photos</h1>
+        <Photo />
+        <Photo />
+      </section>
+  );
+}
